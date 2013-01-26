0b16e9cde886c033bc1379ec04eda1868f1d26fa
Humberto Pinheiro <humberto@MacBook-Pro-de-Usuario.local>
2013-01-26 10:44:54 -0200

    Initial Commit

---
+++ sudoku/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  sudoku
+//
+//  Created by Humberto Pinheiro on 26/01/13.
+//  Copyright (c) 2013 Humberto Pinheiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
